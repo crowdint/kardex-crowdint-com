7ed8b766cfab7c1c98831f2a8de27b80d7798965
David Padilla <david@crowdint.com>
2014-04-09 14:06:05 -0500

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 KardexCrowdintCom::Application.routes.draw do
+  devise_scope :user do
+    get 'google_apps_sign_in', :to => "crowdint_auth/omniauth_callbacks#google_apps_sign_in"
+  end
+
+  devise_for :users, controllers: { omniauth_callbacks: 'crowdint_auth/omniauth_callbacks' }
+
   namespace :admin do
     resources :achievables
   end
