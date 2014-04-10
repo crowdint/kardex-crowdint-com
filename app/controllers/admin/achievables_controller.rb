4a295904b71212f0c51837dc9be5dead192bb49f
David Padilla <david@crowdint.com>
2014-04-10 10:50:59 -0500

    Metaprogramming the achievables controller

--- app/controllers/admin/achievables_controller.rb
+++ app/controllers/admin/achievables_controller.rb
@@ -1,2 +1,60 @@
 class Admin::AchievablesController < Admin::BaseController
+  before_filter :load_object, if: -> { params[:id] }
+
+  respond_to :html
+
+  def index
+    instance_variable_set(collection_name, object_class.all)
+    respond_with instance_variable_get(collection_name)
+  end
+
+  def new
+    instance_variable_set(object_name, object_class.new)
+    respond_with instance_variable_get(object_name)
+  end
+
+  def create
+    instance_variable_set(object_name, object_class.new(protected_params))
+    instance_variable_get(object_name).save
+    respond_with instance_variable_get(object_name), location: index_path
+  end
+
+  def edit
+  end
+
+  def update
+    instance_variable_get(object_name).update_attributes protected_params
+    instance_variable_get(object_name).save
+    respond_with instance_variable_get(object_name), location: index_path
+  end
+
+  def destroy
+    instance_variable_get(object_name).destroy
+    respond_with instance_variable_get(object_name), location: index_path
+  end
+
+  private
+  def protected_params
+    params.require(resource_name.to_sym).permit(:name, :description, :image, user_ids: [])
+  end
+
+  def load_object
+    instance_variable_set(object_name, object_class.find(params[:id]))
+  end
+
+  def collection_name
+    "@#{resource_name.pluralize}"
+  end
+
+  def object_name
+    "@#{resource_name}"
+  end
+
+  def object_class
+    resource_name.classify.constantize
+  end
+
+  def index_path
+    call("admin_#{resource_name.pluralize}_path")
+  end
 end
--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,42 +1,9 @@
-class Admin::BadgesController < Admin::BaseController
-  before_filter :load_badge, if: -> { params[:id] }
-
-  respond_to :html
-
-  def index
-    respond_with @badges = Badge.all
+class Admin::BadgesController < Admin::AchievablesController
+  def resource_name
+    "badge"
   end
 
-  def new
-    respond_with @badge = Badge.new
-  end
-
-  def create
-    @badge = Badge.new(badge)
-    @badge.save
-    respond_with @badge, location: admin_badges_path
-  end
-
-  def edit
-  end
-
-  def update
-    @badge.update_attributes badge
-    @badge.save
-    respond_with @badge, location: admin_badges_path
-  end
-
-  def destroy
-    @badge.destroy
-    respond_with @badge, location: admin_badges_path
-  end
-
-  private
-  def badge
-    params.require(:badge).permit(:name, :description, :image, user_ids: [])
-  end
-
-  def load_badge
-    @badge = Badge.find(params[:id])
+  def index_path
+    admin_badges_path
   end
 end
