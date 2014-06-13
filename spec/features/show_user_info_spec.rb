0bc4ca2284c26a1ab56e83b41fec5bf73d842660
Victor Velazquez <victor.velazquez@crowdint.com>
2014-06-13 03:10:28 -0500

    Add failling case capybara test

--- spec/features/show_user_info_spec.rb
+++ spec/features/show_user_info_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
-feature 'Viewing info page' do
-  let(:user) { Fabricate(:user) }
+feature "Signing in" do
+    background do
+      puts user_omniauth_authorize_path(:google_apps)
+      Capybara.current_driver = :selenium
+    end
 
-  background do
-    visit root_path
-    OmniAuth.config.mock_auth[:google] = {
-      provider: 'google',
-      uid: user.
-    }
-  end
-
-  scenario 'Checking user info' do
-  end
+    scenario "Signing in with incorrect credentials" do
+      visit  user_omniauth_authorize_path(:google_apps)
+      fill_in "Email", with: 'test@crowdint.com'
+      fill_in "Password", with: 'test'
+      click_button 'Sign in'
+      expect(page).to have_content 'The email or password you entered is incorrect.'
+    end
 end
