35413f91a507d001303ee398ff40e92846e9257f
Juan Carlos <zalapeach@gmail.com>
2014-06-13 16:22:00 -0500

    comments tests

--- spec/features/show_user_info_spec.rb
+++ spec/features/show_user_info_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
 feature "Signing in" do
-    background do
-      puts user_omniauth_authorize_path(:google_apps)
-      Capybara.current_driver = :selenium
-    end
 
-    scenario "Signing in with incorrect credentials" do
-      visit  user_omniauth_authorize_path(:google_apps)
-      fill_in "Email", with: 'test@crowdint.com'
-      fill_in "Password", with: 'test'
-      click_button 'Sign in'
-      expect(page).to have_content 'The email or password you entered is incorrect.'
-    end
+  let!(:user) { Fabricate(:user) }
+
+  background do
+    Capybara.current_driver = :selenium
+    visit user_omniauth_authorize_path(:google_apps)
+  end
+
+  scenario "Signing in with incorrect credentials" do
+    # PENDING
+    #fill_in "Email", with: 'test@crowdint.com'
+    #fill_in "Password", with: 'test'
+    #click_button 'Sign in'
+    #expect(page).to have_content 'The email or password you entered is incorrect.'
+  end
+
+  scenario "Signing in with correct credentials" do
+    # PENDING
+    #@auth_hash = double(info: { email: user.email, name: user.name })
+    #@request = stub(env: { 'omniauth.auth' => @auth_hash })
+    #user.stub(:request).and_return(@request)
+  end
 end
