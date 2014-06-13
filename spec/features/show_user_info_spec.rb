7c8b13cdcedafe68c03f44cf23b281a64c6c16f6
Victor Velazquez <victor.velazquez@crowdint.com>
2014-06-13 16:38:52 -0500

    Adding pending capybara test until we fix login issues

--- spec/features/show_user_info_spec.rb
+++ spec/features/show_user_info_spec.rb
@@ -4,21 +4,19 @@ feature "Signing in" do
 
   let!(:user) { Fabricate(:user) }
 
-  background do
-    Capybara.current_driver = :selenium
-    visit user_omniauth_authorize_path(:google_apps)
-  end
+  #background do
+    #Capybara.current_driver = :selenium
+    #visit user_omniauth_authorize_path(:google_apps)
+  #end
 
-  scenario "Signing in with incorrect credentials" do
-    # PENDING
+  xscenario "Signing in with incorrect credentials" do
     #fill_in "Email", with: 'test@crowdint.com'
     #fill_in "Password", with: 'test'
     #click_button 'Sign in'
     #expect(page).to have_content 'The email or password you entered is incorrect.'
   end
 
-  scenario "Signing in with correct credentials" do
-    # PENDING
+  xscenario "Signing in with correct credentials" do
     #@auth_hash = double(info: { email: user.email, name: user.name })
     #@request = stub(env: { 'omniauth.auth' => @auth_hash })
     #user.stub(:request).and_return(@request)
