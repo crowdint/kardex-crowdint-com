16e0c73cb521026595103406e6479f68616e59ff
Juan Carlos <zalapeach@gmail.com>
2014-06-16 10:54:48 -0500

    [https://trello.com/c/1wPlNX51/45-3-score-card-info-user]
    * Adds migration to users table to add position id
    * Adds migration to position table to remove user id
    * Change db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612171610) do
+ActiveRecord::Schema.define(version: 20140613230320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,10 @@ ActiveRecord::Schema.define(version: 20140612171610) do
   end
 
   create_table "positions", force: true do |t|
-    t.string  "name"
-    t.string  "description"
-    t.string  "image_url"
-    t.integer "user_id"
+    t.string "name"
+    t.string "description"
+    t.string "image_url"
   end
-
-  add_index "positions", ["user_id"], name: "index_positions_on_user_id", using: :btree
 
   create_table "skills", force: true do |t|
     t.string   "name"
@@ -70,9 +67,11 @@ ActiveRecord::Schema.define(version: 20140612171610) do
     t.string   "department"
     t.string   "picture_url"
     t.text     "bio"
+    t.integer  "position_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["position_id"], name: "index_users_on_position_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
