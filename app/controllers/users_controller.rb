45200f8f07a33e4dd5788f70f543d99c40c9d378
Juan Carlos <zalapeach@gmail.com>
2014-06-16 11:03:39 -0500

    [https://trello.com/c/1wPlNX51/45-3-score-card-info-user]
    * Adds edit and update logic to users_controller
    * Adds private method user_params to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     respond_with @user
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      respond_with @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :bio, :position_id, skill_ids: [])
+  end
 end
