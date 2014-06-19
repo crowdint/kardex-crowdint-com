65c7c7c3cf8af19e35d5aeb795e092758d36e071
Juan Carlos <zalapeach@gmail.com>
2014-06-19 12:05:09 -0500

    [https://trello.com/c/V1q0dzpL/13-1-5-create-badges-admin] create badges admin
    * Change database.example.yml
    * pending spec in feature badges spec file
    * Adds current coverage

--- spec/features/badges_spec.rb
+++ spec/features/badges_spec.rb
@@ -11,15 +11,15 @@ feature 'badges' do
 
   # This test is failing, for some extrange reason in the
   # browser find a record while in the console doesn't exists
-  scenario 'creating a badge' do
-    #visit badges_engine.badges_path
-    #click_link 'New Badge'
-    #fill_in 'Name', with: 'Dummy Badge'
-    #fill_in 'Description', with: Faker::Lorem.paragraph
-    #fill_in 'Level', with: 1
-    #click_button 'Create Badge'
-    #expect(page).to have_content BadgesEngine::Badge.first.name
-    #expect(page).to have_content BadgesEngine::Badge.first.description
-    #expect(page).to have_content BadgesEngine::Badge.first.level
+  xscenario 'creating a badge' do
+    visit badges_engine.badges_path
+    click_link 'New Badge'
+    fill_in 'Name', with: 'Dummy Badge'
+    fill_in 'Description', with: Faker::Lorem.paragraph
+    fill_in 'Level', with: 1
+    click_button 'Create Badge'
+    expect(page).to have_content BadgesEngine::Badge.first.name
+    expect(page).to have_content BadgesEngine::Badge.first.description
+    expect(page).to have_content BadgesEngine::Badge.first.level
   end
 end
