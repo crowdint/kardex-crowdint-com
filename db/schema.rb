f5d8dff15838c89511ea3e3d69d8adae73b6ed86
Juan Carlos <zalapeach@gmail.com>
2014-06-24 17:06:16 -0500

    Values Catalog Integration
    * Updates badges engine
    * Deletes coverage files
    * Copies migrations from badges engine
    * Updates db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624024603) do
+ActiveRecord::Schema.define(version: 20140624210855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,9 +61,18 @@ ActiveRecord::Schema.define(version: 20140624024603) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "award_id"
+    t.integer  "value_id"
   end
 
   add_index "badges_engine_badges", ["award_id"], name: "index_badges_engine_badges_on_award_id", using: :btree
+  add_index "badges_engine_badges", ["value_id"], name: "index_badges_engine_badges_on_value_id", using: :btree
+
+  create_table "badges_engine_values", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "positions", force: true do |t|
     t.string "name"
