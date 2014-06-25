42e8973eb543053e2b6c141d9f2f6168d1d23617
Juan Carlos <zalapeach@gmail.com>
2014-06-25 18:35:14 -0500

    Create nominee monthly list
    * Adds integration test for create nominee list

---
+++ spec/features/nominee_list/create_nominee_list_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'Create a nominee list' do
+  let!(:badge) { Fabricate(:badge) }
+
+  background do
+    login_with_oauth
+    User.first.update_attributes(is_admin: true)
+    login_with_oauth
+  end
+
+  scenario 'creating a nominee list' do
+    user = User.first
+    visit admin_root_path
+    click_link 'Create nominee list'
+    select badge.name, from: 'Badge'
+    check user.name
+    click_button 'Create Nominee list'
+    expect(page).to have_content 'Nominee list created successfully'
+  end
+end
