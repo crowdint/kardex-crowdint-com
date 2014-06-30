e5890bbc4cb12459ad0941b5cf43fc3634357f61
Juan Carlos <zalapeach@gmail.com>
2014-06-30 11:26:45 -0500

    [https://trello.com/c/mDDc5kpd/55-2-propose-a-badge]
    * Creates new model called propose badge
    * Updates db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626174146) do
+ActiveRecord::Schema.define(version: 20140627181235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,16 @@ ActiveRecord::Schema.define(version: 20140626174146) do
     t.string "description"
     t.string "image_url"
   end
+
+  create_table "propose_badges", force: true do |t|
+    t.string   "name"
+    t.integer  "value_id"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "propose_badges", ["value_id"], name: "index_propose_badges_on_value_id", using: :btree
 
   create_table "skills", force: true do |t|
     t.string   "name"
