df417dcd58447881aff296ec530ee7105ce31054
Juan Carlos <zalapeach@gmail.com>
2014-06-30 11:40:56 -0500

    [https://trello.com/c/mDDc5kpd/55-2-propose-a-badge]
    * Creates propose badge controller
    * Creates propose badge views
    * Adds link to user show page
    * Change header title to nomine users new view

---
+++ app/controllers/propose_badges_controller.rb
@@ -0,0 +1,20 @@
+class ProposeBadgesController < ApplicationController
+  def new
+    @propose_badge = ProposeBadge.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+
+  def propose_badge_params
+    params.require(:propose_badge).permit(
+      :name,
+      :value_id,
+      :description,
+      user_ids: []
+    )
+  end
+end
