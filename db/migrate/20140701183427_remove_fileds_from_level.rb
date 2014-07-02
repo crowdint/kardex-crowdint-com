23f36ac5e65fefa4f36214059077de42c414b1ab
Victor Velazquez <victor.velazquez@crowdint.com>
2014-07-02 12:05:14 -0500

    Removing no existent fields

--- db/migrate/20140701183427_remove_fileds_from_level.rb
+++ db/migrate/20140701183427_remove_fileds_from_level.rb
@@ -1,7 +1,5 @@
 class RemoveFiledsFromLevel < ActiveRecord::Migration
   def change
-    remove_column :badges_engine_levels, :name
-    remove_column :badges_engine_levels, :description
     add_column :badges_engine_levels, :tier, :integer
     add_column :badges_engine_levels, :badge_alias, :string
   end
