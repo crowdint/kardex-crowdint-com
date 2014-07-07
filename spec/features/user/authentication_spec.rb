a0562f4dcb770b7af57ebb1fc109a06dff050671
Victor Velazquez <victor.velazquez@crowdint.com>
2014-07-07 06:04:03 -0500

    Fix broken testing about to hide users index for users

--- spec/features/user/authentication_spec.rb
+++ spec/features/user/authentication_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 feature "User Authentication" do
-  background { login_with_oauth }
+  background do
+    login_with_oauth
+    User.first.update_attributes(is_admin: true)
+    login_with_oauth
+  end
 
   scenario "User is able to signing successfully" do
-    visit users_index_path
-    expect(page).to have_content 'Users'
+    visit admin_root_path
+    expect(page).to have_content 'Admin settings'
   end
 end
