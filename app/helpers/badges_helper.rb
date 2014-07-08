61adc0558d613b37e1f9dbbcbaf7801a73df3efc
Juan Carlos <zalapeach@gmail.com>
2014-07-08 12:26:02 -0500

    Badge view
    * Adds method in badges helper
    * Adds specs for badges helper
    * Adds link to badges#show in badge partial

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -2,4 +2,12 @@ module BadgesHelper
   def badge_totals(user)
     "#{ user.badges.size }/#{ @badges.size }"
   end
+
+  def current_badge(badge)
+    if badge == @badge
+      'row badge-sidebar-current-badge'
+    else
+      'row badge-sidebar-badge'
+    end
+  end
 end
--- spec/helpers/badges_helpers_spec.rb
+++ spec/helpers/badges_helpers_spec.rb
@@ -12,4 +12,20 @@ describe BadgesHelper do
       expect(helper.badge_totals current_user).to eq '1/2'
     end
   end
+
+  describe '#current_badge' do
+    let(:badge_1) { Fabricate.build :badge, name: 'badge 1' }
+    let(:badge_2) { Fabricate.build :badge, name: 'badge 2' }
+
+    it 'returns current badge class if badges are equal' do
+      @badge = badge_1
+      expect(helper.current_badge(badge_1)).
+        to eq 'row badge-sidebar-current-badge'
+    end
+    it 'returns normal badge class if badges are not equal' do
+      @badge = badge_1
+      expect(helper.current_badge(badge_2)).
+        to eq 'row badge-sidebar-badge'
+    end
+  end
 end
