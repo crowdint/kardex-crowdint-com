6087d3dd5eb2c503834f5593a73fec946a2e49bf
Juan Carlos <zalapeach@gmail.com>
2014-07-15 14:17:02 -0500

    Create skills rake task
    * Creates skill_type model
    * Adds relationships between skills and skill types
    * Ads specs for skill and skill types models

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,13 +1,5 @@
-# == Schema Information
-#
-# Table name: skills
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class Skill < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  belongs_to :skill_type
 end
