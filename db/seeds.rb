a913253ecd3eaf15c6185d8f5a423919bd935c83
Juan Carlos <zalapeach@gmail.com>
2014-07-17 16:12:37 -0500

    create rake task for awards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+data = YAML.load_file('config/dataseeds/awards.yml')
+
+data.each do |info|
+  award = BadgesEngine::Award.find_or_initialize_by(title: info['title'])
+  award.description = info['description']
+  award.save
+end
