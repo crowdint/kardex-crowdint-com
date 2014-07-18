7fb7934754483fa0ea46d661f70006e605589ce0
Juan Carlos <zalapeach@gmail.com>
2014-07-18 12:16:05 -0500

    Add icon owned
    * Create helper method in application helper to manage the icon
    * Adds specs for that helper
    * Add method to partial

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   let!(:current_user) { Fabricate :user }
-  let!(:badge_3) { Fabricate :badge, name: 'foo' }
+  let!(:badge) { Fabricate :badge, name: 'foo' }
 
   describe '#show_badges' do
-    xit 'returns a message if badges array is empty' do
-      Fabricate :badge
-      @badges = BadgesEngine::Badge.all
+    it 'returns a message if badges array is empty' do
+      @badges = []
       expect(helper.show_badges).to eq 'You don\'t have badges... yet!'
     end
-    xit 'renders a partial if badges array is not empty' do
+    it 'renders a partial if badges array is not empty' do
+      allow(helper).to receive(:current_user).and_return(current_user)
       @badges = BadgesEngine::Badge.all
       expect(helper.show_badges).to render_template 'shared/_badge'
     end
@@ -31,14 +31,36 @@ describe ApplicationHelper do
 
   describe '#unknown_image?' do
     it 'returns nil name if the badge belongs to the user' do
-      current_user.badges << badge_3
+      current_user.badges << badge
       allow(helper).to receive(:current_user).and_return(current_user)
-      expect(helper.unknown_image? badge_3).to eq nil
+      expect(helper.unknown_image? badge).to eq nil
     end
 
     it 'returns the class name unless the badge belongs to the user' do
       allow(helper).to receive(:current_user).and_return(current_user)
-      expect(helper.unknown_image? badge_3).to eq 'unknown-image'
+      expect(helper.unknown_image? badge).to eq 'unknown-image'
+    end
+  end
+
+  describe '#badge_earn_icon?' do
+    before do
+      params = { action: 'show', controller: 'badges' }
+      allow(helper).to receive(:params).and_return(params)
+      allow(helper).to receive(:current_user).and_return(current_user)
+    end
+
+    context 'badge owned and controller is badges' do
+      it 'returns a span element' do
+        badge.users << current_user
+        expect(helper.badge_earn_icon(badge)).
+          to eq(content_tag :div, nil, class: 'badge-earned-icon')
+      end
+    end
+
+    context 'badge is not owned or controller is not badges' do
+      it 'returns nil' do
+        expect(helper.badge_earn_icon(badge)).to eq nil
+      end
     end
   end
 end
