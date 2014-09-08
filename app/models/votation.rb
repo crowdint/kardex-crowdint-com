6fc2042b4f8b9edaafebd0736cd6bf56f1d2c480
Juan Carlos <zalapeach@gmail.com>
2014-09-08 12:24:37 -0500

    Adds voting functionality
    * Fix name issues in votation model
    * Fix name issues in votation controller
    * Adds fabricator for votation

--- app/models/votation.rb
+++ app/models/votation.rb
@@ -2,18 +2,18 @@ class Votation < ActiveRecord::Base
   belongs_to :badge, class_name: 'BadgesEngine::Badge'
 
   validates :badge_id, presence: true
-  validate :current_month?
+  validate :current_votation?
 
-  before_create :set_is_open
+  before_create :open_votation_process
 
   private
 
-  def set_is_open
+  def open_votation_process
     self.is_open = true
   end
 
-  def current_month?
-    if Voting.where(created_at: self.created_at, badge_id: self.badge_id).any?
+  def current_votation?
+    if Votation.where(created_at: self.created_at, badge_id: self.badge_id).any?
       errors.add(:created_at, 'You already create this votation')
     end
   end
