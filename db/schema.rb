814a4e8b5c888386147606fe0b8362dff250a516
Juan Carlos Aguilar Rincon <juan.aguilar@crowdint.com>
2014-09-26 17:10:16 -0500

    Improvements
    * Adds DS_Store files to .gitignore
    * Update db/schema.rb file
    * Adds skills:create rake task to seeds.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910234906) do
+ActiveRecord::Schema.define(version: 20140916020103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,17 @@ ActiveRecord::Schema.define(version: 20140910234906) do
 
   add_index "propose_badges_users", ["propose_badge_id"], name: "index_propose_badges_users_on_propose_badge_id", using: :btree
   add_index "propose_badges_users", ["user_id"], name: "index_propose_badges_users_on_user_id", using: :btree
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", id: false, force: true do |t|
+    t.integer "role_id", null: false
+    t.integer "user_id", null: false
+  end
 
   create_table "skill_types", force: true do |t|
     t.string   "name"
