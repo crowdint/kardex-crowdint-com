927ed4ab9cc40b86d06f32d8f67c03dc972762d6
Julian Morales Cardenas <julian.morales@crowdint.com>
2014-10-13 12:20:49 -0500

    remove redundant self

--- app/decorators/models/show_dont_tells_engine/show_dont_tell_decorator.rb
+++ app/decorators/models/show_dont_tells_engine/show_dont_tell_decorator.rb
@@ -2,6 +2,6 @@ ShowDontTellsEngine::ShowDontTell.class_eval do
   scope :active, -> { where('date >= ?', Time.now) }
 
   def formated_date
-    self.date.to_date.to_formatted_s(:long)
+    date.to_date.to_formatted_s(:long)
   end
 end
