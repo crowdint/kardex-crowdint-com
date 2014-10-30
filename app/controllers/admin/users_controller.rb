2d019a4b4ac7c1e17137925c513dd970b38f061c
Julian Morales Cardenas <julian.morales@crowdint.com>
2014-10-30 09:51:13 -0600

    add access restriction to engines application controllers

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   helper_method :sort_column, :sort_direction
+  before_filter :restrict_access, :get_last_module
 
   def index
     @users = if params[:search]
@@ -19,5 +20,18 @@ class Admin::UsersController < Admin::BaseController
 
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def restrict_access
+    redirect_to session[:last_moudle] unless current_user.roles.include?(badges_admin) or
+      current_user.is_admin
+  end
+
+  def get_last_module
+    session[:last_moudle] = request.url
+  end
+
+  def badges_admin
+    Role.find_by_name('badges admin')
   end
 end
