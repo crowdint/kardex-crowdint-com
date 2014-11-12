5609fd3c80ff1d0e43d72c33b4df745ca4b7098e
Julian Morales Cardenas <julian.morales@crowdint.com>
2014-11-12 13:25:36 -0600

    fix coments

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -2,13 +2,16 @@ class PresentationsController < ApplicationController
   layout 'dashboards'
 
   def index
-    @presentations = if params[:filter]
-      PresentationsEngine::Presentation.where("date = ?", params[:filter].to_date).
+    @presentations =
+    if params[:filter]
+      PresentationsEngine::Presentation.
+        where("date = ?", params[:filter].to_date).
         order('date')
     elsif params[:search]
       PresentationsEngine::Presentation.joins(:user).
-        where("presentations_engine_presentations.title ILIKE ? OR users.name ILIKE ?",
-        "%#{params[:search]}%", "%#{params[:search]}%")
+        where("presentations_engine_presentations.title ILIKE ? OR
+          users.name ILIKE ?", 
+          "%#{params[:search]}%", "%#{params[:search]}%")
     else
       PresentationsEngine::Presentation.all
     end
--- app/decorators/models/prensentations_engine/presentation_decorator.rb
+++ app/decorators/models/prensentations_engine/presentation_decorator.rb
@@ -1,7 +1,8 @@
 PresentationsEngine::Presentation.class_eval do
   belongs_to :user
 
-  scope :next_wednesday, -> { where( 'date = ?',
-	  Date.today.wday == 3 ? Date.today : Date.today + 1 + ( 2 - (Date.today.wday) ) % 7) }
+  scope :next_wednesday,
+    -> { where('date = ?', Date.today.wday == 3 ?
+  	Date.today : Date.today + 1 + ( 2 - (Date.today.wday)) % 7) }
   scope :active, -> { where('date >= ?', Date.today) }
 end
