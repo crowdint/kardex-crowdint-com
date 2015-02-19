61ebac38e8b65407a24b322b4150f9eba3202f57
Victor Velazquez <victor.velazquez@crowdint.com>
2015-02-19 17:59:17 -0600

    Rename votations to vote event model + fabricator

--- app/models/vote_event.rb
+++ app/models/vote_event.rb
@@ -1,24 +1,24 @@
-class Votation < ActiveRecord::Base
+class VoteEvent < ActiveRecord::Base
   belongs_to :badge, class_name: 'BadgesEngine::Badge'
   has_many :votes, dependent: :destroy
   has_many :users, through: :votes
 
   validates :badge_id, presence: true
 
-  validate :current_votation?
+  validate :current_vote_event?
 
-  before_create :open_votation_process
+  before_create :open_vote_event_process
 
   scope :active?, ->(value = true) { where(is_open: value) }
 
   private
 
-  def open_votation_process
+  def open_vote_event_process
     self.is_open = true
   end
 
-  def current_votation?
-    if Votation.where(date: date, badge_id: badge_id).any?
+  def current_vote_event?
+    if VoteEvent.where(date: date, badge_id: badge_id).any?
       errors.add(:created_at, 'You already create this votation')
     end
   end
