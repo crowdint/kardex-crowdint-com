232ca65900540ab4f2a8ceb754c915ab6c7393e8
Victor Velazquez <victor.velazquez@crowdint.com>
2015-03-02 13:06:18 -0600

    Check houndci comments:
    * Space after closing | missing.
    * Useless private access modifier.
    * Space inside parentheses detected.
    * Trailing whitespace detected.

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,9 +25,9 @@ class User < ActiveRecord::Base
 
   scope :admins, -> { where(is_admin: true) }
   scope :ordered, -> { order('name ASC') }
-  scope :order_by_votes, ->(votes) {
+  scope :order_by_votes, -> (votes) {
     order_ids = order_ids_by_votes(votes, :desc)
-    where( id: order_ids).order(build_sentence(order_ids))
+    where(id: order_ids).order(build_sentence(order_ids))
   }
 
   def to_param
@@ -42,18 +42,17 @@ class User < ActiveRecord::Base
     roles.pluck(:name).include? "admin_#{current_module}"
   end
 
-  private
   def self.order_ids_by_votes(votes, order)
     if order == :desc
-      votes.sort_by{|k,v|v}.reverse.to_h.keys
+      votes.sort_by{|_k,v| v}.reverse.to_h.keys
     else
-      votes.sort_by{|k,v|v}.to_h.keys
+      votes.sort_by{|_k,v| v}.to_h.keys
     end
   end
 
   def self.build_sentence(order_ids)
     sentence = order_ids.each_with_index.map do |id, index|
-      "WHEN #{id} THEN #{index}" 
+      "WHEN #{id} THEN #{index}"
     end.join(' ')
     "CASE id #{sentence} END"
   end
